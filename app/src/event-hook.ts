5a3d8bde5dffadd5647ebe617c94bb9ce79f0a8e
Daniel Dyla <dyladan@users.noreply.github.com>
2024-09-06 09:00:47 -0400

    Add error

--- app/src/event-hook.ts
+++ app/src/event-hook.ts
@@ -1,6 +1,7 @@
-import { EvaluationDetails, FlagValue, Hook, HookContext, HookHints } from '@openfeature/server-sdk';
+import { EvaluationDetails, FlagValue, Hook, HookContext, HookHints, OpenFeatureError } from '@openfeature/server-sdk';
 import { EventLogger, events } from "@opentelemetry/api-events";
-import { ATTR_FEATURE_FLAG_KEY, ATTR_FEATURE_FLAG_PROVIDER_NAME, ATTR_FEATURE_FLAG_VARIANT, EVENT_NAME_FEATURE_FLAG_EVALUATION } from './proposed-attributes';
+import { ATTR_FEATURE_FLAG_ERROR_CODE, ATTR_FEATURE_FLAG_ERROR_MESSAGE, ATTR_FEATURE_FLAG_KEY, ATTR_FEATURE_FLAG_PROVIDER_NAME, ATTR_FEATURE_FLAG_VARIANT, EVENT_NAME_FEATURE_FLAG_EVALUATION } from './proposed-attributes';
+import { Attributes } from '@opentelemetry/api';
 
 export class EventHook implements Hook {
     private _el: EventLogger;
@@ -21,6 +22,29 @@ export class EventHook implements Hook {
     }
 
     error(hookContext: Readonly<HookContext<FlagValue>>, error: unknown, hookHints?: HookHints): void | Promise<void> {
-        hookContext.defaultValue
+        const attributes: Attributes = {
+            [ATTR_FEATURE_FLAG_KEY]: hookContext.flagKey,
+            [ATTR_FEATURE_FLAG_PROVIDER_NAME]: hookContext.providerMetadata.name,
+            // default variant doesn't have a name
+            // should we insert the actual default value?
+            [ATTR_FEATURE_FLAG_VARIANT]: "default variant",
+        };
+        if (error instanceof OpenFeatureError) {
+            attributes[ATTR_FEATURE_FLAG_ERROR_CODE] = error.code;
+            attributes[ATTR_FEATURE_FLAG_ERROR_MESSAGE] = error.message;
+        } else if (error instanceof Error) {
+            // TODO general may not be the right choice
+            attributes[ATTR_FEATURE_FLAG_ERROR_CODE] = "GENERAL";
+            attributes[ATTR_FEATURE_FLAG_ERROR_MESSAGE] = error.message;
+        } else {
+            // this is just a guess tbh
+            attributes[ATTR_FEATURE_FLAG_ERROR_CODE] = "GENERAL";
+            attributes[ATTR_FEATURE_FLAG_ERROR_MESSAGE] = String(error);
+        }
+
+        this._el.emit({
+            name: EVENT_NAME_FEATURE_FLAG_EVALUATION,
+            attributes,
+        });
     }
 }
--- app/src/proposed-attributes.ts
+++ app/src/proposed-attributes.ts
@@ -3,3 +3,6 @@ export const EVENT_NAME_FEATURE_FLAG_EVALUATION = "feature_flag.evaluation";
 export const ATTR_FEATURE_FLAG_KEY = "feature_flag.key";
 export const ATTR_FEATURE_FLAG_PROVIDER_NAME = "feature_flag.provider_name";
 export const ATTR_FEATURE_FLAG_VARIANT = "feature_flag.variant";
+
+export const ATTR_FEATURE_FLAG_ERROR_CODE = "feature_flag.evaluation.error.code";
+export const ATTR_FEATURE_FLAG_ERROR_MESSAGE = "feature_flag.evaluation.error.message";
